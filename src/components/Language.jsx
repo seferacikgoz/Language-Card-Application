e4d7c57862bf6ef42b57bc8a1d23165de037b185
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-23 00:28:01 +0300

    v3

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -3,9 +3,10 @@ import Image from "react-bootstrap/Image"
 const Language = ({name, img, options}) => {
 console.log(name, img, options)
   return (
-   <Container>
+   <Container style={{background: "peachpuff"}} className="p-4 rounded-4">
     <Container>
     <Image src={img} width="70%"></Image>
+    <h3 className="display-6">{name}</h3>
     </Container>
    </Container>
   )
